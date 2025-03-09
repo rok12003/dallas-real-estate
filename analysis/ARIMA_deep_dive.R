28901adde3bc31ab94cd747861102dd58907d3c7
rok12003 <rohitkandala97@gmail.com>
2025-03-08 19:00:05 -0500

    [DEV]: finished up arima deep dive

--- analysis/ARIMA_deep_dive.R
+++ analysis/ARIMA_deep_dive.R
@@ -12,36 +12,71 @@ library(zoo)
 library(urca)
 library(feasts)
 
+### Increasing DPI so it doesn't look like the 90s:
+options(repr.plot.res = 200)
+
 ### Loading in data:
 dallas_housing_long <- readRDS("data/processed_dfs/dallas_housing_long.rds")
 dallas_forecast_simple <- readRDS("data/processed_dfs/dallas_forecast_simple.rds")
 
-# Seeing if Dec 2024/Jan 2025 (predicted) actually hold up to Dec 2024/Jan 2025 (actual)
-## Initializing the validation df:
-validation_df <- dallas_forecast_simple |> 
+### Creating dataframe that has predicted & actual vals:
+#### Predicted df:
+predicted_df <- dallas_forecast_simple |> 
   filter(Date <= yearmonth("2025-01")) |>
   rename(Prediction = Price)
 
-## Reading in just the columns w/ Dec 2024 & Jan 2025 data:
-actual_vals <- dallas_housing_long |>
+#### Actual vals:
+actual_df <- dallas_housing_long |>
   filter(Date > '2024-11-30') |>
   select(RegionName, Date, Price) |>
-  
-  #### Converting the date to year-month format:
+
+  ##### Converting the date to year-month format:
   mutate(Date = yearmonth(Date))
 
-### Joining validation_df to actual_vals:
-validation_df <- inner_join(validation_df, actual_vals, by = c(
-  "RegionName", "Date"))
+#### Joining them:
+comparision_df <- inner_join(predicted_df, actual_df, by = c("RegionName", "Date")) |>
+ 
+  #### Rounding:
+  mutate(Price = round(Price, 0)) |>
+  mutate(Prediction = round(Prediction, 0)) |>
+  
+  #### Creating Residual column:
+  mutate(Residual = Price - Prediction)
 
-### Creating a column that calculates the difference between predicted & actual:
-validation_df <- validation_df |> 
-  mutate(diff = Prediction - Price) |> 
-  arrange(desc(diff)) 
+# Understanding if ARIMA is a good predictor!
+## First we'll calculate some errors:
+### Mean Absolute Error:
+arima_mae <- mean(abs(comparision_df$Residual))
 
-### HOLY SHIT--only an average of $363 difference between ARIMA & actual price
-### Linear model kinda goated ngl. 
+### Mean Squared Error:
+arima_mse <- mean((comparision_df$Residual)^2)
 
-# Look at distribution
-# Does it vary greatly by zip code? 
-# How's it working under the hood?
+### Root Mean Squared Error:
+arima_rmse <- sqrt(arima_mse)
+
+### Mean Absolute Percentage Error:
+arima_mape <- mean(abs((comparision_df$Residual)
+                       /comparision_df$Price)) * 100
+
+### Creating a well-formatted output:
+metrics_text <- paste0(
+  "MAE: $", format(round(arima_mae, 2), big.mark = ","), "\n",
+  "RMSE: $", format(round(arima_rmse, 2), big.mark = ","), "\n",
+  "MAPE: ", format(round(arima_mape, 2), nsmall = 2), "%"
+)
+
+## Plotting:
+## Residual vs. Fitted Plot:
+p <- ggplot(comparision_df, aes(x = Prediction, y = Residual)) +
+  geom_point(alpha = 0.5) +  
+  geom_smooth(method = "lm", color = "red", se = FALSE) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "blue") +
+  labs(title = "Residuals vs. Predicted Values",
+       x = "Predicted Price",
+       y = "Residual") +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  annotate("text", x = min(comparision_df$Prediction), 
+           y = min(comparision_df$Residual) * 0.9, 
+           label = metrics_text, 
+           hjust = 0, size = 4, color = "black")
